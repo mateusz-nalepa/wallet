6e9b599f6afc59b1298f5c7a44e77354d0926928
Mateusz Nalepa <mateusz.nalepa@softylabs.com>
2019-04-18 20:17:18 +0200

    initial menu

--- app/src/main/java/com/mateuszcholyn/wallet/MainActivity.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.mateuszcholyn.wallet
 
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -15,9 +18,21 @@ const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
+    private var drawerLayout : DrawerLayout?  = null
+    private var toggle: ActionBarDrawerToggle? = null
+
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout?.addDrawerListener(toggle!!)
+        toggle?.syncState()
+        supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
@@ -35,19 +50,24 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        // as you specify a parent activity in AndroidManifest.x ml.
+        if (item.itemId == R.id.action_settings) {
+            return true
+//            else -> super.onOptionsItemSelected(item)
         }
+
+        if (toggle!!.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
-//        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-//            putExtra(EXTRA_MESSAGE, message)
-//        }
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
         startActivity(intent)
     }
 
