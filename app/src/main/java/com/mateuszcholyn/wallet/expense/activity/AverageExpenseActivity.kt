8e4c2beeb58f5fcc7f60097fb181ca842e4f130b
Mateusz Nalepa <mateusz.nalepa@softylabs.com>
2019-06-06 22:22:01 +0200

    initial average expense

--- app/src/main/java/com/mateuszcholyn/wallet/expense/activity/AverageExpenseActivity.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/expense/activity/AverageExpenseActivity.kt
@@ -1,38 +1,86 @@
 package com.mateuszcholyn.wallet.expense.activity
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import com.github.salomonbrys.kodein.KodeinInjector
 import com.github.salomonbrys.kodein.android.AppCompatActivityInjector
 import com.github.salomonbrys.kodein.instance
 import com.mateuszcholyn.wallet.R
+import com.mateuszcholyn.wallet.category.service.CategoryService
+import com.mateuszcholyn.wallet.config.ApplicationContext
 import com.mateuszcholyn.wallet.expense.adapter.AverageExpenseAdapter
 import com.mateuszcholyn.wallet.expense.service.ExpenseService
+import com.mateuszcholyn.wallet.util.HourChooser
 import com.mateuszcholyn.wallet.util.Tablica
+import com.mateuszcholyn.wallet.util.simpleDateFormat
+import java.util.*
 
 class AverageExpenseActivity : AppCompatActivity(), AppCompatActivityInjector {
 
     override val injector: KodeinInjector = KodeinInjector()
     private val expenseService: ExpenseService by instance()
+    private val categoryService: CategoryService by instance()
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var activity: Activity
+    private var beginCalendar: Calendar = Calendar.getInstance()
+    private var endCalendar: Calendar = Calendar.getInstance()
+    private lateinit var beginDate: TextView
+    private lateinit var endDate: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initializeInjector()
+        activity = this
         setContentView(R.layout.activity_average_expense)
+        initCategorySpinner()
+        initDateTimePickers()
+    }
 
-        recyclerView = findViewById(R.id.average_expense_recycler_view)
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = AverageExpenseAdapter(Tablica.napisy())
+    private fun initDateTimePickers() {
+        initBeginDateTimePicker()
+        initEndDateTimePicker()
+    }
 
-        recyclerView = recyclerView.apply {
-            layoutManager = viewManager
-            adapter = viewAdapter
+    private fun initBeginDateTimePicker() {
+        beginDate = findViewById(R.id.average_begin_dateTimePicker)
+        val gregorianCalendar = GregorianCalendar()
+        gregorianCalendar.add(Calendar.DAY_OF_WEEK, -7)
+        beginDate.text = simpleDateFormat.format(gregorianCalendar.time)
+        HourChooser(beginCalendar, activity, beginDate)
+    }
+
+    private fun initEndDateTimePicker() {
+        endDate = findViewById(R.id.average_end_dateTimePicker)
+        endDate.text = simpleDateFormat.format(GregorianCalendar().time)
+        HourChooser(endCalendar, activity, endDate)
+    }
+
+    fun calculateAverageAmount(view: View) {
+        Toast
+                .makeText(ApplicationContext.appContext, "Liczę wartość...", Toast.LENGTH_LONG)
+                .show()
+    }
+
+    private fun initCategorySpinner() {
+
+        val spinner: Spinner = findViewById(R.id.average_category_spinner)
+        val lista = categoryService.getAllNamesOnly()
+
+        ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                lista
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
         }
     }
 
