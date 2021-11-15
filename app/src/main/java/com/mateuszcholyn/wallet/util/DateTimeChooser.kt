517ce57c750c152498dccb8955f85c7608c188ce
Mateusz Nalepa <mateusz.nalepa@softylabs.com>
2021-11-15 20:37:01 +0100

    fix showing month value

--- app/src/main/java/com/mateuszcholyn/wallet/util/DateTimeChooser.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/util/DateTimeChooser.kt
@@ -18,19 +18,28 @@ class DateTimeChooser(
     private var time: LocalTime = LocalTime.now()
     private var date: LocalDate = LocalDate.now()
 
-    private val textListener = View.OnClickListener {
+    private val textListener =
+        View.OnClickListener {
+            showDatePickerDialog()
+        }
+
+    private fun showDatePickerDialog() {
         DatePickerDialog(
             activity,
             mDateDataSet,
             now.year,
-            now.monthValue,
+            now.fixedMonthValueMinusOne(),
             now.dayOfMonth
         )
             .show()
     }
 
+    private fun LocalDateTime.fixedMonthValueMinusOne(): Int {
+        return this.monthValue - 1
+    }
+
     private val mDateDataSet = DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
-        date = LocalDate.of(year, month, day)
+        date = LocalDate.of(year, month + 1, day)
 
         TimePickerDialog(
             activity,
