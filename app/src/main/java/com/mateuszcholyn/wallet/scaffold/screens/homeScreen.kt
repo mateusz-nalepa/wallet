e3fb511fa42f26c4d979a63b61b651befb346b92
Mateusz Nalepa <mateusz.nalepa@softylabs.com>
2022-01-07 20:40:17 +0100

    add secondary text: ilosc wydatkow

--- app/src/main/java/com/mateuszcholyn/wallet/scaffold/screens/homeScreen.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/scaffold/screens/homeScreen.kt
@@ -69,6 +69,7 @@ fun HomeScreen() {
             items(categoryList) { categoryModel ->
                 ListItem(
                         text = { Text(categoryModel.name) },
+                        secondaryText = { Text("Ilość wydatków: TBD") },
                         trailing = {
                             IconButton(
                                     onClick = {
