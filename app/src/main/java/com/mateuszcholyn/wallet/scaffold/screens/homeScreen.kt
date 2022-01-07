47c8a3c4e76b3d4d37e0a0fd039bdecc0b5bb816
Mateusz Nalepa <mateusz.nalepa@softylabs.com>
2022-01-07 17:59:41 +0100

    working dummy elements from material design

--- app/src/main/java/com/mateuszcholyn/wallet/scaffold/screens/homeScreen.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/scaffold/screens/homeScreen.kt
@@ -1,38 +1,97 @@
 package com.mateuszcholyn.wallet.scaffold.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.mateuszcholyn.wallet.R
+import com.mateuszcholyn.wallet.scaffold.util.defaultButtonModifier
+import com.mateuszcholyn.wallet.scaffold.util.defaultModifier
+import com.mateuszcholyn.wallet.view.showShortText
 
 @Composable
 fun HomeScreen() {
-    Column(
-            modifier = Modifier
-                    .fillMaxSize()
-                    .background(colorResource(id = R.color.colorPrimaryDark))
-                    .wrapContentSize(Alignment.Center)
-    ) {
-        Text(
-                text = "Home View",
-                fontWeight = FontWeight.Bold,
-                color = Color.White,
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                textAlign = TextAlign.Center,
-                fontSize = 25.sp
-        )
+
+    var text by remember { mutableStateOf("Hello") }
+    var text2 by remember { mutableStateOf("Hello") }
+
+    Column {
+        Row(
+                modifier = defaultModifier,
+        ) {
+
+            OutlinedTextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text("Label") },
+                    modifier = defaultModifier,
+            )
+        }
+
+        Row(
+                modifier = defaultModifier,
+        ) {
+
+            OutlinedTextField(
+                    value = text2,
+                    onValueChange = { text2 = it },
+                    label = { Text("Label2") },
+                    modifier = defaultModifier,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+            )
+        }
+
+        Row(modifier = defaultModifier) {
+            Button(
+                    onClick = {
+                        showShortText("CLICKED XD")
+                    },
+                    modifier = defaultButtonModifier,
+
+                    ) {
+                Text("Button")
+            }
+        }
     }
+
+
+//    Button(
+//            onClick = {
+//                      showShortText("XDDDDDD")
+//            },
+//            // Uses ButtonDefaults.ContentPadding by default
+//    ) {
+//        // Inner content including an icon and a text label
+//        Icon(
+//                Icons.Filled.Favorite,
+//                contentDescription = "Favorite",
+//                modifier = Modifier.size(ButtonDefaults.IconSize)
+//        )
+//        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+//        Text("Like")
+//    }
+
+
+//    Column(
+//            modifier = Modifier
+//                    .fillMaxSize()
+//                    .background(colorResource(id = R.color.colorPrimaryDark))
+//                    .wrapContentSize(Alignment.Center)
+//    ) {
+//        Text(
+//                text = "Home View",
+//                fontWeight = FontWeight.Bold,
+//                color = Color.White,
+//                modifier = Modifier.align(Alignment.CenterHorizontally),
+//                textAlign = TextAlign.Center,
+//                fontSize = 25.sp
+//        )
+//    }
 }
 
 @Preview(showBackground = true)
