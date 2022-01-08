09ebfafdd4e51acdba4f1f09e8b54f441c2d69bd
Mateusz Nalepa <mateusz.nalepa@softylabs.com>
2022-01-09 00:26:08 +0100

    restore old view

--- app/src/main/java/com/mateuszcholyn/wallet/MainActivity.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/MainActivity.kt
@@ -1,95 +1,93 @@
 package com.mateuszcholyn.wallet
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.mateuszcholyn.wallet.config.ApplicationContext
-import com.mateuszcholyn.wallet.scaffold.MainScreen
-import org.kodein.di.compose.withDI
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import org.kodein.di.DIAware
+import org.kodein.di.android.closestDI
 
 
-class MainActivity : AppCompatActivity() {
-    @ExperimentalMaterialApi
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContent {
-            withDI(di = ApplicationContext.appDi) {
-                MaterialTheme {
-                    ProvideWindowInsets {
-                        val systemUiController = rememberSystemUiController()
-                        val darkIcons = MaterialTheme.colors.isLight
-                        SideEffect {
-                            systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = darkIcons)
-                        }
-                        MainScreen()
-                    }
-                }
-            }
-
-        }
-    }
-}
-
-
-//class MainActivity : AppCompatActivity(), DIAware, NavigationView.OnNavigationItemSelectedListener {
-//
-//    lateinit var drawer: DrawerLayout
-//
-//    override val di by closestDI()
-//
+//class MainActivity : AppCompatActivity() {
+//    @ExperimentalMaterialApi
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        toggle()
-//        switchToSummaryFragment()
-//        navigationState(savedInstanceState)
-//    }
 //
-//    private fun toggle() {
-//        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-//        setSupportActionBar(toolbar)
+//        setContent {
+//            withDI(di = ApplicationContext.appDi) {
+//                MaterialTheme {
+//                    ProvideWindowInsets {
+//                        val systemUiController = rememberSystemUiController()
+//                        val darkIcons = MaterialTheme.colors.isLight
+//                        SideEffect {
+//                            systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = darkIcons)
+//                        }
+//                        MainScreen()
+//                    }
+//                }
+//            }
 //
-//        drawer = findViewById(R.id.drawer_layout)
-//
-//        val toggle =
-//                ActionBarDrawerToggle(
-//                        this, drawer, toolbar,
-//                        R.string.navigation_drawer_open, R.string.navigation_drawer_close
-//                )
-//
-//        drawer.addDrawerListener(toggle)
-//
-//        toggle.syncState()
-//    }
-//
-//    private fun navigationState(savedInstanceState: Bundle?) {
-//        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-//        navigationView.setNavigationItemSelectedListener(this)
-//
-//        if (savedInstanceState == null) {
-//            navigationView.setCheckedItem(R.id.nav_message)
-//        }
-//    }
-//
-//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//        handleNavigation(item)
-//
-//        drawer.closeDrawer(GravityCompat.START)
-//        return true
-//    }
-//
-//    override fun onBackPressed() {
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START)
-//        } else {
-//            super.onBackPressed()
 //        }
 //    }
 //}
+
+
+class MainActivity : AppCompatActivity(), DIAware, NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var drawer: DrawerLayout
+
+    override val di by closestDI()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toggle()
+        switchToSummaryFragment()
+        navigationState(savedInstanceState)
+    }
+
+    private fun toggle() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        val toggle =
+                ActionBarDrawerToggle(
+                        this, drawer, toolbar,
+                        R.string.navigation_drawer_open, R.string.navigation_drawer_close
+                )
+
+        drawer.addDrawerListener(toggle)
+
+        toggle.syncState()
+    }
+
+    private fun navigationState(savedInstanceState: Bundle?) {
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.nav_message)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        handleNavigation(item)
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
