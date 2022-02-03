8ffd3a2c4a0aa3883ced31b63b02c0f932bb21dc
Mateusz Nalepa <mateusz.nalepa@softylabs.com>
2022-02-03 21:09:30 +0100

    remove unused function

--- app/src/androidTest/java/com/mateuszcholyn/wallet/random.kt
+++ app/src/androidTest/java/com/mateuszcholyn/wallet/random.kt
@@ -2,7 +2,6 @@ package com.mateuszcholyn.wallet
 
 import com.mateuszcholyn.wallet.domain.category.Category
 import com.mateuszcholyn.wallet.domain.expense.Expense
-import com.mateuszcholyn.wallet.infrastructure.category.CategoryEntity
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.util.*
@@ -11,11 +10,6 @@ fun randomString(): String = UUID.randomUUID().toString()
 
 fun randomCategoryName(): String = "categoryName-${randomString()}"
 fun randomDescription(): String = "description-${randomString()}"
-
-fun randomNewCategoryEntity(): CategoryEntity =
-        CategoryEntity(
-                name = randomCategoryName()
-        )
 
 fun randomNewCategory(): Category =
         Category(
