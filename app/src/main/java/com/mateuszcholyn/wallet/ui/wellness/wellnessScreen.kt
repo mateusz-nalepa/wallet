95f3655ebecce8da0258e05ab0eb65de7a683c13
Mateusz Nalepa <mateuszcholyn@gmail.com>
2022-07-05 23:13:07 +0200

    https://developer.android.com/codelabs/jetpack-compose-state tutorial - restore state in compose

--- app/src/main/java/com/mateuszcholyn/wallet/ui/wellness/wellnessScreen.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/ui/wellness/wellnessScreen.kt
@@ -1,11 +1,14 @@
 package com.mateuszcholyn.wallet.ui.wellness
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -23,7 +26,7 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text("You've had $count glasses.")
         }
