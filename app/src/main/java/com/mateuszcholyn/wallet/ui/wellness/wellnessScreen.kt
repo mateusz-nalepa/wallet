ee7b4888e418fd05d54a9d86dca2d48e4331e874
Mateusz Nalepa <mateuszcholyn@gmail.com>
2022-07-05 23:08:38 +0200

    https://developer.android.com/codelabs/jetpack-compose-state tutorial - remember in composition

--- app/src/main/java/com/mateuszcholyn/wallet/ui/wellness/wellnessScreen.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/ui/wellness/wellnessScreen.kt
@@ -1,6 +1,7 @@
 package com.mateuszcholyn.wallet.ui.wellness
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -18,24 +19,47 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier)
 }
 
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            // This text is present if the button has been clicked
-            // at least once; absent otherwise
             Text("You've had $count glasses.")
         }
-        Button(
-                onClick = { count++ },
-                modifier = Modifier.padding(top = 8.dp),
-                enabled = count < 10
-        ) {
-            Text(text = "Add one")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
+
+//Remember in composition
+//@Composable
+//fun WaterCounter(modifier: Modifier = Modifier) {
+//
+//    Column(modifier = modifier.padding(16.dp)) {
+//        var count by remember { mutableStateOf(0) }
+//        if (count > 0) {
+//            var showTask by remember { mutableStateOf(true) }
+//            if (showTask) {
+//                WellnessTaskItem(
+//                        taskName = "Have you taken your 15 minute walk today?",
+//                        onClose = { showTask = false },
+//                )
+//            }
+//            Text("You've had $count glasses.")
+//        }
+//        Row(Modifier.padding(top = 8.dp)) {
+//            Button(
+//                    onClick = { count++ },
+//                    enabled = count < 10
+//            ) {
+//                Text(text = "Add one")
+//            }
+//            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+//                Text("Clear water count")
+//            }
+//        }
+//    }
+//}
 
