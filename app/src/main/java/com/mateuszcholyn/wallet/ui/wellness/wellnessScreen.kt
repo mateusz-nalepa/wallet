e04c73b16e0251e64a3d0ece6ee414dbfab8d9ee
Mateusz Nalepa <mateuszcholyn@gmail.com>
2022-07-05 22:59:46 +0200

    https://developer.android.com/codelabs/jetpack-compose-state tutorial - state driven ui

--- app/src/main/java/com/mateuszcholyn/wallet/ui/wellness/wellnessScreen.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/ui/wellness/wellnessScreen.kt
@@ -24,10 +24,15 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
 
-        Text(text = "You've had $count glasses")
+        if (count > 0) {
+            // This text is present if the button has been clicked
+            // at least once; absent otherwise
+            Text("You've had $count glasses.")
+        }
         Button(
                 onClick = { count++ },
                 modifier = Modifier.padding(top = 8.dp),
+                enabled = count < 10
         ) {
             Text(text = "Add one")
         }
