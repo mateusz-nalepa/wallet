c737b1ebd032526ef746d73f658103e8e476325b
Mateusz Nalepa <mateuszcholyn@gmail.com>
2022-07-30 11:01:06 +0200

    remove useless code

--- app/src/main/java/com/mateuszcholyn/wallet/ui/screen/summary/advancedFilters/AdvancedFiltersSection.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/ui/screen/summary/advancedFilters/AdvancedFiltersSection.kt
@@ -2,13 +2,11 @@ package com.mateuszcholyn.wallet.ui.screen.summary.advancedFilters
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import com.mateuszcholyn.wallet.R
 import com.mateuszcholyn.wallet.ui.util.defaultModifier
 
@@ -21,15 +19,6 @@ fun AdvancedFiltersSection() {
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-
-        ClickableText(
-            text = if (advancedFiltersExpanded) AnnotatedString(stringResource(R.string.hideFilters)) else AnnotatedString(
-                stringResource(R.string.showFilters)
-            ),
-            onClick = {
-                advancedFiltersExpanded = !advancedFiltersExpanded
-            },
-        )
         Text(
             text =
             if (advancedFiltersExpanded)
