c9b910db7d5faf70b61e3c5bcc479bd0b9444542
Mateusz Nalepa <mateuszcholyn@gmail.com>
2022-08-04 19:56:56 +0200

    verify if exception is thrown when trying to remove non existing expense

--- app/src/test/java/com/mateuszcholyn/wallet/tests/usecase/removeexpense/RemoveExpenseUseCaseTest.kt
+++ app/src/test/java/com/mateuszcholyn/wallet/tests/usecase/removeexpense/RemoveExpenseUseCaseTest.kt
@@ -1,10 +1,14 @@
 package com.mateuszcholyn.wallet.tests.usecase.removeexpense
 
+import com.mateuszcholyn.wallet.backend.expensecore.ExpenseNotFoundException
+import com.mateuszcholyn.wallet.catchThrowable
+import com.mateuszcholyn.wallet.randomExpenseId
 import com.mateuszcholyn.wallet.tests.manager.ExpenseScope
 import com.mateuszcholyn.wallet.tests.manager.ext.removeExpenseUseCase
 import com.mateuszcholyn.wallet.tests.manager.initExpenseAppManager
 import com.mateuszcholyn.wallet.tests.manager.validator.validate
 import com.mateuszcholyn.wallet.usecase.ExpenseRemovedStatus
+import com.mateuszcholyn.wallet.validate
 import org.junit.Test
 
 class RemoveExpenseUseCaseTest {
@@ -35,5 +39,31 @@ class RemoveExpenseUseCaseTest {
         }
     }
 
+    @Test
+    fun `should throw exception when trying to remove non existing category`() {
+        // given
+        val nonExistingExpenseId = randomExpenseId()
+        val manager =
+            initExpenseAppManager {
+                category {
+                    expense { }
+                }
+            }
+
+        // when
+        val throwable =
+            catchThrowable {
+                manager.removeExpenseUseCase {
+                    expenseId = nonExistingExpenseId
+                }
+            }
+
+
+        // then
+        throwable.validate {
+            isInstanceOf(ExpenseNotFoundException::class)
+            hasMessage("Expense with id ${nonExistingExpenseId.id} does not exist")
+        }
+    }
 
 }
---
+++ app/src/test/java/com/mateuszcholyn/wallet/throwableValidator.kt
@@ -0,0 +1,35 @@
+package com.mateuszcholyn.wallet
+
+import kotlin.reflect.KClass
+
+fun catchThrowable(throwableAction: () -> Unit): Throwable {
+    try {
+        throwableAction()
+    } catch (t: Throwable) {
+        return t
+    }
+
+    throw RuntimeException("Expected exception, but nothing has been thrown")
+}
+
+fun Throwable.validate(validationBlock: ThrowableValidator.() -> Unit) {
+    ThrowableValidator(this).apply(validationBlock)
+}
+
+class ThrowableValidator(
+    private val t: Throwable,
+) {
+
+    fun isInstanceOf(expectedClass: KClass<out Throwable>) {
+        assert(t::class == expectedClass) {
+            "Expected throwable class is: $expectedClass. Actual: ${t::class}"
+        }
+    }
+
+    fun hasMessage(expectedMessage: String) {
+        assert(t.message == expectedMessage) {
+            "Expected message is: [$expectedMessage]. Actual: [${t.message}]"
+        }
+    }
+
+}
