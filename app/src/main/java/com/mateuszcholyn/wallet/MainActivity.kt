92ab83114d4cf4b673955b31c02202cfd0056d62
Mateusz Nalepa <mateuszcholyn@gmail.com>
2024-01-21 14:09:22 +0100

    splash screen is working

--- app/src/main/java/com/mateuszcholyn/wallet/MainActivity.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/MainActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Colors
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
@@ -24,6 +24,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
@@ -40,7 +43,8 @@ import kotlinx.coroutines.flow.map
 @Composable
 fun LoadingScreen() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "ładowanie XD", color = MaterialTheme.colors.primary)
+        val image: Painter = painterResource(id = R.drawable.ic_launcher_foreground)
+        Image(painter = image, contentDescription = "App Icon")
     }
 }
 
@@ -73,6 +77,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         verifyStoragePermissions(this)
 
+        installSplashScreen().setKeepOnScreenCondition {
+            SelectedColorsMutableState.value == null
+        }
+
         setContent {
             MyApp()
         }
@@ -80,12 +88,6 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-fun ustawKolorkiXD(isSystemInDarkTheme: Boolean): String =
-    if (isSystemInDarkTheme) {
-        "dark"
-    } else {
-        "light"
-    }
 
 
 private fun resolveThemeV2(actualTheme: String?): Colors =
@@ -123,9 +125,7 @@ fun MyApp() {
     }
 
 
-    if (isLoading) {
-        LoadingScreen()
-    } else {
+//    if (!isLoading) {
         CompositionLocalProvider(LocalIsLightThemeComposition provides rememberSaveable { SelectedColorsMutableState }) {
             // Teraz możesz użyć LocalIsLightTheme w dowolnym miejscu w Twojej aplikacji
 
@@ -137,7 +137,10 @@ fun MyApp() {
                 }
             }
         }
-
-    }
+//    } else {
+//        LoadingScreen()
+//
+//
+//    }
 
 }
