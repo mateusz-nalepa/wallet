6f6b6fc93a090d3ff354b39a7b969a7697c12b90
Mateusz Nalepa <mateuszcholyn@gmail.com>
2024-01-21 14:16:57 +0100

    miejsce gdzie w kodzie jest topbar XD

--- app/src/main/java/com/mateuszcholyn/wallet/frontend/view/skeleton/topBar.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/frontend/view/skeleton/topBar.kt
@@ -1,12 +1,20 @@
 package com.mateuszcholyn.wallet.frontend.view.skeleton
 
-import androidx.compose.material.*
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.primarySurface
+import androidx.compose.material.rememberDrawerState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -19,7 +27,12 @@ import kotlinx.coroutines.launch
 fun TopBar(scope: CoroutineScope, scaffoldState: ScaffoldState) {
     TopAppBar(
         title = {
-            Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
+            Text(
+//                text = stringResource(R.string.app_name),
+                text = "kolorowe XD",
+                fontSize = 18.sp
+            )
+        },
         navigationIcon = {
             IconButton(onClick = {
                 scope.launch {
