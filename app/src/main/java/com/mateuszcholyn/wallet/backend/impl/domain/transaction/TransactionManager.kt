2af11088b96b0a376c7006ac8f5c39592f397d7e
Mateusz Nalepa <mateuszcholyn@gmail.com>
2024-02-22 10:58:01 +0100

    add todos

--- app/src/main/java/com/mateuszcholyn/wallet/backend/impl/domain/transaction/TransactionManager.kt
+++ app/src/main/java/com/mateuszcholyn/wallet/backend/impl/domain/transaction/TransactionManager.kt
@@ -1,5 +1,6 @@
 package com.mateuszcholyn.wallet.backend.impl.domain.transaction
 
+// TODO: przetestuj to jakoś XD
 interface TransactionManager {
     suspend fun <T> runInTransaction(block: suspend () -> T): T
 
